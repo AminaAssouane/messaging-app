1a2bedf2b6156c433bc8376ecb0e16fa64fa24f3
Amina <minassouane@gmail.com>
2026-03-19 20:31:05 +0100

    fix(auth): correct imports/exports

--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const authRouter = Router();
 const authController = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 authRouter.post("/register", authController.register);
 authRouter.post("/login", authController.login);
+authRouter.get("/me", authMiddleware, (req, res) => {
+  res.json(req.user);
+});
 
-module.exports = { authRouter };
+module.exports = authRouter;
