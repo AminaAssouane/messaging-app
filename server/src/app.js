8c274f1a57b6326555a88d269ae24c22b62853b7
Amina <minassouane@gmail.com>
2026-03-19 17:10:43 +0100

    feat(server): create express server

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
+const authRouter = require("./routes/authRouter");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(3000, (error) => {
+  if (error) console.log("Error occured : ", error);
+  else console.log(`Server running on port ${PORT}`);
+});
