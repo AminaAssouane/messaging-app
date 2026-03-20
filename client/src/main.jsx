47de67a4774449ea1fe96f99ddeb3e0794880a60
Amina <minassouane@gmail.com>
2026-03-20 14:39:10 +0100

    feat(auth): implement register page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import { Register } from "./pages/Register/Register.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  { path: "/auth/register", element: <Register /> },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <App router={router} />
   </StrictMode>,
-)
+);
---
+++ client/src/pages/Register/Register.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import api from "../../services/api";
+import styles from "./Register.module.css";
+
+export function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await api.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.error(error.response?.data || error.message);
+    }
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username : </label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="email">Email : </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password : </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+}
