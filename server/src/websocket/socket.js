8a59489a541c40bfc141a76996a5a1e57101012c
Amina <minassouane@gmail.com>
2026-03-20 06:51:52 +0100

    feat(server): add joining global chat automatically

--- server/src/websocket/socket.js
+++ server/src/websocket/socket.js
@@ -4,6 +4,10 @@ function initSockets(io) {
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
+    // automatically join global chat
+    socket.join(1);
+    console.log(`Socket ${socket.id} joined global chat`);
+
     socket.on("join_conversation", (conversationId) => {
       socket.join(conversationId);
       console.log(`Socket ${socket.id} joined conversation ${conversationId}`);
