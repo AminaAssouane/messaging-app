e4d6f72a776227751ab8167a9f43cef0272e3f72
Amina <minassouane@gmail.com>
2026-03-20 06:09:03 +0100

    feat(server): initialize socket.io with cors support

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 require("dotenv/config");
 const express = require("express");
+const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 const authRouter = require("./routes/authRouter");
 const conversationRouter = require("./routes/conversationRouter");
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(cors());
 app.use(express.json());
@@ -13,8 +16,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/auth", authRouter);
 app.use("/conversations", conversationRouter);
 
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(3000, (error) => {
+server.listen(3000, (error) => {
   if (error) console.log("Error occured : ", error);
   else console.log(`Server running on port ${PORT}`);
 });
