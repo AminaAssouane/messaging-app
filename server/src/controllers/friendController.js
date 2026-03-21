b7cf407cd6d9497cc9208191b9a98674eb4ad865
Amina <minassouane@gmail.com>
2026-03-21 22:19:40 +0100

    feat(friends): add set friend request controller

--- server/src/controllers/friendController.js
+++ server/src/controllers/friendController.js
@@ -13,7 +13,38 @@ async function getFriendRequests(req, res) {
   }
 }
 
-async function sendFriendRequest(req, res) {}
+async function sendFriendRequest(req, res) {
+  const senderId = req.user.userId;
+  const { username } = req.body;
+  try {
+    const target = await prisma.user.findUnique({ where: { username } });
+    if (!target) return res.status(404).json({ message: "User not found" });
+    if (target.id === senderId)
+      return res.status(400).json({ message: "Can't add yourself" });
+
+    const existing = await prisma.friendRequest.findFirst({
+      where: {
+        OR: [
+          { senderId, receiverId: target.id },
+          { senderId: target.id, receiverId: senderId },
+        ],
+      },
+    });
+    if (existing)
+      return res.status(400).json({ message: "Request already exists" });
+
+    const request = await prisma.friendRequest.create({
+      data: {
+        senderId,
+        receiverId: targetId,
+        status: "PENDING",
+      },
+    });
+    res.status(201).json(request);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
 
 async function acceptFriendRequest(req, res) {}
 
@@ -21,4 +52,4 @@ async function rejectFriendRequest(req, res) {}
 
 async function getFriends(req, res) {}
 
-module.exports = { getFriendRequests };
+module.exports = { getFriendRequests, sendFriendRequest };
