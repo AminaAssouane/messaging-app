f7b4d0a7280f8d2cd3cd1f1ee114e64cd4e8e101
Amina <minassouane@gmail.com>
2026-03-23 03:50:56 +0100

    style(groups): add styling to the InviteMemberModal

--- client/src/components/InviteMemberModal/InviteMemberModal.jsx
+++ client/src/components/InviteMemberModal/InviteMemberModal.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { inviteMember } from "../../services/api";
 import styles from "./InviteMemberModal.module.css";
+import { UserPlus } from "lucide-react";
 
 export default function InviteMemberModal({ groupId, friends, onClose }) {
   const [status, setStatus] = useState({});
@@ -18,21 +19,24 @@ export default function InviteMemberModal({ groupId, friends, onClose }) {
   return (
     <div className={styles.modalOverlay}>
       <div className={styles.modal}>
-        <h2>Invite Member</h2>
+        <h2 className={styles.header}>Invite Member</h2>
         <ul>
           {friends.map((f) => (
-            <li key={f.id}>
+            <li key={f.id} className={styles.friend}>
               {f.username}
               <button
                 onClick={() => handleInvite(f.id)}
                 disabled={!!status[f.id]}
+                className={styles.addUserBtn}
               >
-                {status[f.id] ?? "Invite"}
+                {status[f.id] ?? <UserPlus />}
               </button>
             </li>
           ))}
         </ul>
-        <button onClick={onClose}>Close</button>
+        <button onClick={onClose} className={styles.closeBtn}>
+          Close
+        </button>
       </div>
     </div>
   );
